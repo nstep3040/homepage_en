e5463dfef7132759780d348e3a06ae21c6ead4d0
nstep3040 <77074266+lKori@users.noreply.github.com>
2022-09-07 16:40:22 +0900

    20220907

---
+++ js/lnb_sonar.js
@@ -0,0 +1,31 @@
+document.write(`
+<div id="sonarLnb" class="lnb">
+    <h2>Sonar System</h2>
+    <ul>
+        <li>
+            <input type="radio" id="sonarSub01" name="sonarSub" />
+            <label for="sonarSub01"><a href="./sub_sonar_01.html">Side Scan Sonar MB</a></label>
+        </li>
+        <li>
+            <input type="radio" id="sonarSub02" name="sonarSub" />
+            <label for="sonarSub02"><a href="./sub_sonar_02.html">Side Scan Sonar SB</a></label>
+        </li>
+        <li>
+            <input type="radio" id="sonarSub03" name="sonarSub" />
+            <label for="sonarSub03"><a href="./sub_sonar_03.html">BathyThemograph</a></label>
+        </li>
+        <li>
+            <input type="radio" id="sonarSub04" name="sonarSub" />
+            <label for="sonarSub04"><a href="./sub_sonar_04.html">Naval Depth Sounder</a></label>
+        </li>
+        <li>
+            <input type="radio" id="sonarSub05" name="sonarSub" />
+            <label for="sonarSub05"><a href="./sub_sonar_05.html">URN System</a></label>
+        </li>
+        <li>
+            <input type="radio" id="sonarSub06" name="sonarSub" />
+            <label for="sonarSub06"><a href="./sub_sonar_06.html">Sonar Testing System</a></label>
+        </li>
+    </ul>
+</div>
+`);
