fd66e3f05eecae649914be8c590dda50a1049731
nstep3040 <77074266+lKori@users.noreply.github.com>
2022-09-07 16:30:52 +0900

    20220907

---
+++ js/foot.js
@@ -0,0 +1,19 @@
+document.write(`
+<footer class="footer">
+    <div class="footerInfo">
+        <div class="footerLogo"></div>
+        <div class="footerText">
+            <p>
+                대구광역시 북구 유통단지로 16, 산업용재관 23동 4호 (우)41518<br>
+                Tel: <strong>+82-53-604-3040</strong>&nbsp;&nbsp;&nbsp;Fax:
+                <strong>+82-53-604-3042</strong>&nbsp;&nbsp;&nbsp;E-mail: <strong>ns@nstep.co.kr</strong><br>
+                Copyrightⓒ Since 1999 NextStep Co. All rights reserved.
+            </p>
+        </div>
+    </div>
+</footer>
+</div>
+</body>
+
+</html>
+`);
